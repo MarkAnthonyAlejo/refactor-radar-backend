cea349b54b64b66cba8812fac4344f882cc6b9c2
Mark Anthony Alejo <164105917+MarkAnthonyAlejo@users.noreply.github.com>
2025-10-06 14:52:09 -0700

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import analyzeRoute from './routes/analyzeRoute'
-import "./types/tree-sitter-typescript";
+
 
 
 dotenv.config();
